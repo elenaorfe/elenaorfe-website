252c0a92785125710e606c6b133dcdfecfa64b6c
elenaortega <elenaorfe@gmail.com>
2024-03-23 00:24:08 +0100

    Improve scrolling

--- packages/@elenaorfe-client/components/HeaderCoverLetter.tsx
+++ packages/@elenaorfe-client/components/HeaderCoverLetter.tsx
@@ -11,9 +11,9 @@ const HeaderCoverLetter: React.FC = () => {
 	useEffect(() => {
 		const handleScroll = (): void => {
 			const offset = window.scrollY;
-			if (offset > 0) {
+			if (offset > 60 && !isSticky) {
 				setIsSticky(true);
-			} else {
+			} else if (offset === 0) {
 				setIsSticky(false);
 			}
 		};
@@ -23,54 +23,56 @@ const HeaderCoverLetter: React.FC = () => {
 		return () => {
 			window.removeEventListener('scroll', handleScroll);
 		};
-	}, []);
+	}, [isSticky]);
 
 	return (
-		<header
-			className={`sticky top-0 z-50 bg-[--color-background] px-4 ${isSticky ? 'border-b border-[--color-text-shade-03] border-opacity-75 py-2 shadow-lg md:border-b-2' : ''}`}
-		>
-			<div
-				className={`mx-auto max-w-5xl items-center ${isSticky ? 'mx-4 flex flex-wrap gap-2 md:mx-auto md:gap-4' : 'pt-[255px] md:pt-16'}`}
+		<React.Fragment>
+			<header
+				className={`sticky top-0 z-40 bg-[--color-background] px-4 ${isSticky ? 'border-b border-[--color-text-shade-03] border-opacity-75 py-2 shadow-lg md:border-b-2' : ''}`}
 			>
-				<p className={isSticky ? 'text-lg md:text-xl' : 'text-2xl md:text-4xl'}>
-					{aboutData.name}
-				</p>
-				{!isSticky && (
-					<p className="text-2xl text-[--color-primary] md:text-4xl">
-						{aboutData.role}
+				<div
+					className={`mx-auto max-w-5xl items-center ${isSticky ? 'mx-4 flex flex-wrap gap-2 md:mx-auto md:gap-4' : 'mt-16'}`}
+				>
+					<p
+						className={`transition-all ${isSticky ? 'text-lg md:text-xl' : 'text-2xl md:text-4xl'}`}
+					>
+						{aboutData.name}
 					</p>
-				)}
-				<Chip withOpacity={true}>
-					<div className="flex items-center">
-						<strong className="text-xs">Applicant</strong>
-						<svg
-							width={2}
-							height={2}
-							fill="currentColor"
-							aria-hidden="true"
-							className="mx-2"
-						>
-							<circle cx="1" cy="1" r="1"></circle>
-						</svg>
-						<span className="text-xs">Design Engineer</span>
-					</div>
-				</Chip>
-				<ExperienceTile
-					showGeneralExperience={true}
-					skillIds={['reactjs', 'typescript', 'tailwindcss']}
-					isCompact={isSticky}
-				/>
-			</div>
+					{!isSticky && (
+						<p className="text-2xl text-[--color-primary] md:text-4xl">
+							{aboutData.role}
+						</p>
+					)}
+					<Chip withOpacity={true}>
+						<div className="flex items-center">
+							<strong className="text-xs">Applicant</strong>
+							<svg
+								width={2}
+								height={2}
+								fill="currentColor"
+								aria-hidden="true"
+								className="mx-2"
+							>
+								<circle cx="1" cy="1" r="1"></circle>
+							</svg>
+							<span className="text-xs">Design Engineer</span>
+						</div>
+					</Chip>
+					<ExperienceTile
+						showGeneralExperience={true}
+						skillIds={['reactjs', 'typescript', 'tailwindcss']}
+						isCompact={isSticky}
+					/>
+				</div>
+			</header>
 			{!isSticky && (
 				<Image
 					src={profilePic}
 					alt=""
-					width={250}
-					height={250}
-					className="absolute right-0 top-0"
+					className="absolute right-0 top-0 z-50 h-[100px] w-[100px] md:h-[250px] md:w-[250px]"
 				/>
 			)}
-		</header>
+		</React.Fragment>
 	);
 };
 
--- packages/@elenaorfe-client/pages/_document.tsx
+++ packages/@elenaorfe-client/pages/_document.tsx
@@ -2,7 +2,7 @@ import { Head, Html, Main, NextScript } from 'next/document';
 
 export default function Document(): JSX.Element {
 	return (
-		<Html lang="en">
+		<Html lang="en" className="scroll-smooth">
 			<Head>
 				<link rel="manifest" href="/manifest.json" />
 				<link rel="apple-touch-icon" href="/icon.png"></link>
