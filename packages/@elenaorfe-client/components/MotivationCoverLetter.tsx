9d39bd1fd78fc99362513d1596da4b8d83ec39c0
elenaortega <elenaorfe@gmail.com>
2024-03-22 19:01:23 +0100

    Update Motivation section

--- packages/@elenaorfe-client/components/MotivationCoverLetter.tsx
+++ packages/@elenaorfe-client/components/MotivationCoverLetter.tsx
@@ -9,75 +9,12 @@ const MotivationCoverLetter: React.FC = () => {
 				eleifend magna leo, a pulvinar tortor finibus et. Nam cursus, lectus at
 				efficitur scelerisque, leo lectus pellentesque velit, eget efficitur
 				lacus lectus sed ipsum. Aenean augue nibh, efficitur nec dignissim et,
-				tristique sit amet dolor. Sed nec ligula et nisi consectetur placerat ac
-				in felis. Vivamus lacinia at diam quis rutrum. Duis aliquet viverra
-				felis, quis auctor lorem laoreet id. Phasellus ac vestibulum dui.
-				Integer nec lectus elit. In hac habitasse platea dictumst. Etiam sed
-				nisi efficitur mauris molestie tristique at id dolor. Integer facilisis
-				scelerisque dui, ac interdum sem auctor ac. Suspendisse vehicula, nisi
-				eget vulputate pulvinar, sem mauris lobortis augue, at ornare orci
-				turpis in velit. Maecenas nec mollis urna. Nunc ut maximus elit, in
-				porta purus. Nunc volutpat justo quis interdum pellentesque.
+				tristique sit amet dolor.
 			</p>
 			<p className="hyphens-aito mb-4 text-justify">
 				Vivamus aliquet interdum ipsum id ullamcorper. Nam sit amet purus
 				tempus, molestie ipsum in, consectetur ex. Curabitur tincidunt luctus
-				enim, ut lobortis quam tincidunt in. Proin consectetur ligula placerat
-				felis convallis, in varius ligula viverra. Fusce vitae dolor ante.
-				Suspendisse vitae massa quis nulla sodales sodales in quis ligula.
-				Mauris tincidunt vulputate aliquam. Aliquam eu mattis quam. Donec
-				euismod lorem eu dolor dignissim fermentum. Nam suscipit imperdiet orci,
-				eu imperdiet sapien vehicula nec. Fusce scelerisque luctus odio.
-			</p>
-			<p className="hyphens-aito mb-4 text-justify">
-				Lorem ipsum dolor sit amet, consectetur adipiscing elit. Curabitur
-				eleifend magna leo, a pulvinar tortor finibus et. Nam cursus, lectus at
-				efficitur scelerisque, leo lectus pellentesque velit, eget efficitur
-				lacus lectus sed ipsum. Aenean augue nibh, efficitur nec dignissim et,
-				tristique sit amet dolor. Sed nec ligula et nisi consectetur placerat ac
-				in felis. Vivamus lacinia at diam quis rutrum. Duis aliquet viverra
-				felis, quis auctor lorem laoreet id. Phasellus ac vestibulum dui.
-				Integer nec lectus elit. In hac habitasse platea dictumst. Etiam sed
-				nisi efficitur mauris molestie tristique at id dolor. Integer facilisis
-				scelerisque dui, ac interdum sem auctor ac. Suspendisse vehicula, nisi
-				eget vulputate pulvinar, sem mauris lobortis augue, at ornare orci
-				turpis in velit. Maecenas nec mollis urna. Nunc ut maximus elit, in
-				porta purus. Nunc volutpat justo quis interdum pellentesque.
-			</p>
-			<p className="hyphens-aito mb-4 text-justify">
-				Vivamus aliquet interdum ipsum id ullamcorper. Nam sit amet purus
-				tempus, molestie ipsum in, consectetur ex. Curabitur tincidunt luctus
-				enim, ut lobortis quam tincidunt in. Proin consectetur ligula placerat
-				felis convallis, in varius ligula viverra. Fusce vitae dolor ante.
-				Suspendisse vitae massa quis nulla sodales sodales in quis ligula.
-				Mauris tincidunt vulputate aliquam. Aliquam eu mattis quam. Donec
-				euismod lorem eu dolor dignissim fermentum. Nam suscipit imperdiet orci,
-				eu imperdiet sapien vehicula nec. Fusce scelerisque luctus odio.
-			</p>
-			<p className="hyphens-aito mb-4 text-justify">
-				Lorem ipsum dolor sit amet, consectetur adipiscing elit. Curabitur
-				eleifend magna leo, a pulvinar tortor finibus et. Nam cursus, lectus at
-				efficitur scelerisque, leo lectus pellentesque velit, eget efficitur
-				lacus lectus sed ipsum. Aenean augue nibh, efficitur nec dignissim et,
-				tristique sit amet dolor. Sed nec ligula et nisi consectetur placerat ac
-				in felis. Vivamus lacinia at diam quis rutrum. Duis aliquet viverra
-				felis, quis auctor lorem laoreet id. Phasellus ac vestibulum dui.
-				Integer nec lectus elit. In hac habitasse platea dictumst. Etiam sed
-				nisi efficitur mauris molestie tristique at id dolor. Integer facilisis
-				scelerisque dui, ac interdum sem auctor ac. Suspendisse vehicula, nisi
-				eget vulputate pulvinar, sem mauris lobortis augue, at ornare orci
-				turpis in velit. Maecenas nec mollis urna. Nunc ut maximus elit, in
-				porta purus. Nunc volutpat justo quis interdum pellentesque.
-			</p>
-			<p className="hyphens-aito mb-4 text-justify">
-				Vivamus aliquet interdum ipsum id ullamcorper. Nam sit amet purus
-				tempus, molestie ipsum in, consectetur ex. Curabitur tincidunt luctus
-				enim, ut lobortis quam tincidunt in. Proin consectetur ligula placerat
-				felis convallis, in varius ligula viverra. Fusce vitae dolor ante.
-				Suspendisse vitae massa quis nulla sodales sodales in quis ligula.
-				Mauris tincidunt vulputate aliquam. Aliquam eu mattis quam. Donec
-				euismod lorem eu dolor dignissim fermentum. Nam suscipit imperdiet orci,
-				eu imperdiet sapien vehicula nec. Fusce scelerisque luctus odio.
+				enim, ut lobortis quam tincidunt in.
 			</p>
 		</section>
 	);
