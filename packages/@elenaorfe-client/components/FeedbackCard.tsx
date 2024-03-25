6c9a52bfd55245957077c11a279afbff2244f972
elenaortega <elenaorfe@gmail.com>
2024-03-25 19:41:49 +0100

    Refactor FeedbackCard component and update feedback data

--- packages/@elenaorfe-client/components/FeedbackCard.tsx
+++ packages/@elenaorfe-client/components/FeedbackCard.tsx
@@ -11,21 +11,28 @@ interface FeedbackCardProps {
 const FeedbackCard: React.FC<FeedbackCardProps> = ({ feedback }) => {
 	return (
 		<Card>
-			<div className="p-4">
-				<div className="flex h-12 w-12 bg-[url('/assets/img/blob.svg')] bg-no-repeat">
-					<Icon
-						icon="bi:quote"
-						className="my-auto ml-2 h-6 w-6 text-[--color-background]"
-					/>
+			<div
+				className="flex h-full flex-col justify-between p-4
+			"
+			>
+				<div>
+					<div className="flex h-12 w-12 bg-[url('/assets/img/blob.svg')] bg-no-repeat">
+						<Icon
+							icon="bi:quote"
+							className="my-auto ml-2 h-6 w-6 text-[--color-background]"
+						/>
+					</div>
+					<p className="text-persian-green-500 mb-4">{feedback.comment}</p>
 				</div>
-				<p className="text-persian-green-500 mb-4">{feedback.comment}</p>
-				<p className="text-persian-green-500 text-sm font-bold">
-					{feedback.author.name}
-				</p>
-				<p className="text-persian-green-500 text-xs">
-					{feedback.author.role} at {feedback.author.company} on{' '}
-					{formatDate(feedback.date)}
-				</p>
+				<div className="items-end">
+					<p className="text-persian-green-500 text-sm font-bold">
+						{feedback.author.name}
+					</p>
+					<p className="text-persian-green-500 text-xs">
+						{feedback.author.role} at {feedback.author.company} on{' '}
+						{formatDate(feedback.date)}
+					</p>
+				</div>
 			</div>
 		</Card>
 	);
