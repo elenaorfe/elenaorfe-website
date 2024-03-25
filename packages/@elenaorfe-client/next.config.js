7027378855eacba235f9c2bf396868f9cf875935
elenaortega <elenaorfe@gmail.com>
2024-03-25 17:49:55 +0100

    Fix precache manifest missing entries when recompiled via webpack --watch

--- packages/@elenaorfe-client/next.config.js
+++ packages/@elenaorfe-client/next.config.js
@@ -14,4 +14,6 @@ const withPWA = require('next-pwa')({
 	skipWaiting: true,
 });
 
-module.exports = withPWA(nextConfig);
+// See https://github.com/GoogleChrome/workbox/issues/1790
+module.exports =
+	process.env.NODE_ENV === 'development' ? nextConfig : withPWA(nextConfig);
