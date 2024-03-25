7ad3c94d0e7c98c9a033b9996076cf08e298b464
elenaortega <elenaorfe@gmail.com>
2024-03-25 17:36:17 +0100

    Improve global styles

--- packages/@elenaorfe-client/pages/_document.tsx
+++ packages/@elenaorfe-client/pages/_document.tsx
@@ -2,13 +2,13 @@ import { Head, Html, Main, NextScript } from 'next/document';
 
 export default function Document(): JSX.Element {
 	return (
-		<Html lang="en">
+		<Html lang="en" className="m-0 p-0">
 			<Head>
 				<link rel="manifest" href="/manifest.json" />
 				<link rel="apple-touch-icon" href="/icon.png"></link>
 				<meta name="theme-color" content="#fff" />
 			</Head>
-			<body className="bg-[--color-background] text-[--color-text-base]">
+			<body className="m-0 bg-[--color-background] p-0 text-[--color-text-base]">
 				<Main />
 				<NextScript />
 			</body>
