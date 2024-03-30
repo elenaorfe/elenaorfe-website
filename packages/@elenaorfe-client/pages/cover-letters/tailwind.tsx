6b28fceb732198dcb215335ea745c67f1676d3f6
elenaortega <elenaorfe@gmail.com>
2024-03-30 23:57:38 +0100

    Add Open Graph and Twitter meta tags for cover letter

--- packages/@elenaorfe-client/pages/cover-letters/tailwind.tsx
+++ packages/@elenaorfe-client/pages/cover-letters/tailwind.tsx
@@ -20,6 +20,35 @@ const TailwindCSSCoverLetter: React.FC = () => {
 				/>
 				<meta charSet="utf-8" />
 				<meta name="viewport" content="initial-scale=1.0, width=device-width" />
+				<meta property="og:type" content="website"></meta>
+				<meta
+					property="og:url"
+					content="https://www.elenaorfe.com/cover-letters/tailwind"
+				></meta>
+				<meta
+					property="og:title"
+					content="Elena | Applicant · Design Engineer for Tailwind Labs"
+				></meta>
+				<meta
+					property="og:description"
+					content="Cover letter for the Design Engineer position at Tailwind Labs."
+				></meta>
+				<meta
+					property="og:image"
+					content="https://www.elenaorfe.com/assets/img/profile_cover_letter.png"
+				></meta>
+				<meta
+					property="twitter:url"
+					content="https://www.elenaorfe.com/cover-letters/tailwind"
+				></meta>
+				<meta
+					property="twitter:title"
+					content="Elena | Applicant · Design Engineer for Tailwind Labs"
+				></meta>
+				<meta
+					property="twitter:description"
+					content="Cover letter for the Design Engineer position at Tailwind Labs."
+				></meta>
 			</Head>
 			<HeaderCoverLetter />
 			<div className="px-4 py-16 sm:mx-auto sm:max-w-lg md:max-w-2xl lg:max-w-4xl xl:max-w-5xl">
