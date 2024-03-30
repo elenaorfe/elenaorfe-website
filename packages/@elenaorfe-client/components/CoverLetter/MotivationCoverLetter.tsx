4e41fb32e9003e9b3c04973504ca552853764737
elenaortega <elenaorfe@gmail.com>
2024-03-30 18:48:21 +0100

    Add TextUnderlined component for underlining text

--- packages/@elenaorfe-client/components/CoverLetter/MotivationCoverLetter.tsx
+++ packages/@elenaorfe-client/components/CoverLetter/MotivationCoverLetter.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import feedbackData from '../../data/en/feedback.json';
 import FeedbackCard from '../FeedbackCard';
+import TextUnderlined from '../TextUnderlined';
 
 const MotivationCoverLetter: React.FC = () => {
 	return (
@@ -8,21 +9,40 @@ const MotivationCoverLetter: React.FC = () => {
 			<h2 className="mb-2 text-lg font-semibold">
 				What I&apos;m excited about
 			</h2>
-			<p className="mb-4 hyphens-auto text-justify">
-				I want to work in a product-oriented company where I can take
-				<span className="text-persian-green-500"> ownership </span>
+			<p className="mb-4 text-justify">
+				I want to work in a product-oriented company where I can take{' '}
+				<TextUnderlined id="motivation-ownership">
+					<span>ownership</span>
+				</TextUnderlined>{' '}
 				and contribute to its growth through meaningful products. After getting
 				to know TailwindCSS and read the RefactoringUI book some years ago,
 				working with you would be a dream come true!
 			</p>
-			<p className="mb-4 hyphens-auto text-justify">
+			<p className="mb-4 hyphens-manual text-justify">
+				I believe I would be a valuable addition to the team as I am motivated
+				by{' '}
+				<TextUnderlined id="motivation-committed">
+					<span>challenges</span>
+				</TextUnderlined>
+				,{' '}
+				<TextUnderlined id="motivation-committed">
+					<span>committed</span>
+				</TextUnderlined>{' '}
+				and{' '}
+				<TextUnderlined id="motivation-committed">
+					<span>proactive</span>
+				</TextUnderlined>
+				. I&apos;ve been lucky to work with awesome people, some of them have
+				shared their thoughts with me:
+			</p>
+			{/* <p className="mb-4 hyphens-auto text-justify">
 				I believe I would be a valuable addition to the team as I am motivated
 				by<span className="text-persian-green-500"> challenges</span>,
 				<span className="text-persian-green-500"> committed </span>and
 				<span className="text-persian-green-500"> proactive</span>. I&apos;ve
 				been lucky to work with awesome people, some of them have shared their
 				thoughts with me:
-			</p>
+			</p> */}
 			<div className="flex snap-x scroll-pl-4 gap-4 overflow-x-auto py-4">
 				{feedbackData.map((feedback) => (
 					<div
---
+++ packages/@elenaorfe-client/components/TextUnderlined.tsx
@@ -0,0 +1,38 @@
+import React from 'react';
+
+interface TextUnderlinedProps {
+	children: React.ReactNode;
+	id: string;
+}
+
+const TextUnderlined: React.FC<TextUnderlinedProps> = ({ id, children }) => {
+	const [showUnderline, setShowUnderline] = React.useState(false);
+
+	// Show underline when element appears in the viewport
+	React.useEffect(() => {
+		const observer = new IntersectionObserver(
+			([entry]) => {
+				console.log('here', id);
+				setShowUnderline(entry.isIntersecting);
+			},
+			{ rootMargin: '-50px', threshold: 1 },
+		);
+
+		console.log(id);
+		observer.observe(document.getElementById(id) as HTMLElement);
+
+		return () => {
+			observer.disconnect();
+		};
+	}, [id]);
+
+	return (
+		<span
+			id={id}
+			className={`after:bg-persian-green-500 relative after:absolute after:bottom-0 after:left-0 after:h-0.5 after:transition-[width] after:ease-in after:content-[''] ${showUnderline ? 'after:w-full' : 'after:w-0'}`}
+		>
+			{children}
+		</span>
+	);
+};
+export default TextUnderlined;
