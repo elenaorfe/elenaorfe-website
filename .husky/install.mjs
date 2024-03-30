84954add6c2b25991f633fa8c04408c4a8462f19
elenaortega <elenaorfe@gmail.com>
2024-03-30 10:41:54 +0100

    Check variables

--- .husky/install.mjs
+++ .husky/install.mjs
@@ -1,4 +1,6 @@
 // Skip Husky install in production and CI
+console.log('process.env.NODE_ENV', process.env.NODE_ENV);
+console.log('process.env.CI', process.env.CI);
 if (process.env.NODE_ENV !== 'production' && process.env.CI !== 'true') {
 	(await import('husky')).default;
 	console.log('Husky installed 🎉');
