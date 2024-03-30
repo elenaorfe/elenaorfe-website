4940288bc5d779e0edc214dbd9d78ec3558aa6ea
elenaortega <elenaorfe@gmail.com>
2024-03-30 10:52:32 +0100

    Clean code

--- .husky/install.mjs
+++ .husky/install.mjs
@@ -1,7 +1,4 @@
 // Skip Husky install in production and CI
-console.log('process.env.VERCEL_ENV', process.env.VERCEL_ENV);
-console.log('process.env.CI', process.env.CI);
-console.log('!process.env.CI', !process.env.CI);
 if (process.env.NODE_ENV !== 'production' && !process.env.CI) {
 	(await import('husky')).default;
 	console.log('Husky installed 🎉');
